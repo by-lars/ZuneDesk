9ce7b2c2e9e246047b50c3f845490b21bfc11052
Lars <lars@pfrenger.com>
2021-02-07 00:53:43 +0100

    🛠️ Started to add System Media Transport Controls integration

--- README.md
+++ README.md
@@ -6,6 +6,17 @@ NOTE: ZuneDesk is not affiliated with or endorsed by microsoft.
 ## What is ZuneDesk?
 ZuneDesk is a desktop overlay for playing music. It's inspired by the Zune UI. 
 
+## Features
+This is still in very early development, so there aren't many features.
+But the overlay is already somewhat integrated with Windows APIs for getting information about the currently playing media. 
+
+## Plans
+- [x] System Media Transport Controls integration
+- [ ] Audio Visualizer
+- [ ] Spotify integration
+- [ ] Customizations
+- [ ] More of the Zune HD Ui
+
 ## Acknowledgements
-* Thanks to [This reddit post](https://www.reddit.com/r/Zune/comments/l8bu6r/windows_10_zune_start_menu_concept/) for inspiring me to do this!
-* Thanks to microsoft for making the Zune :)
+- Thanks to [this reddit post](https://www.reddit.com/r/Zune/comments/l8bu6r/windows_10_zune_start_menu_concept/) for inspiring me to do this!
+- Thanks to microsoft for making the Zune :)
--- ZuneDesk/MainWindow.xaml.cs
+++ ZuneDesk/MainWindow.xaml.cs
@@ -12,6 +12,11 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Windows.Media.Control;
+using Windows.Foundation;
+using System.Diagnostics;
+using System.IO;
+
 
 namespace ZuneDesk
 {
@@ -20,9 +25,97 @@ namespace ZuneDesk
     /// </summary>
     public partial class MainWindow : Window
     {
+        private GlobalSystemMediaTransportControlsSession session;
+        private GlobalSystemMediaTransportControlsSessionManager sessionManager;
+
         public MainWindow()
         {
+            SetupSession();
             InitializeComponent();
+        }
+
+        void SetupSession()
+        {
+            sessionManager = GlobalSystemMediaTransportControlsSessionManager.RequestAsync().GetAwaiter().GetResult();
+
+            session = sessionManager.GetCurrentSession();
+            session.MediaPropertiesChanged       += Session_MediaPropertiesChanged;
+            session.PlaybackInfoChanged          += Session_PlaybackInfoChanged;
+            session.TimelinePropertiesChanged    += Session_TimelinePropertiesChanged;
+
+            sessionManager.CurrentSessionChanged += SessionManager_CurrentSessionChanged;
+        }
+
+        private void SessionManager_CurrentSessionChanged(GlobalSystemMediaTransportControlsSessionManager sender, CurrentSessionChangedEventArgs args)
+        {
+            session = sender.GetCurrentSession();
+            session.MediaPropertiesChanged      += Session_MediaPropertiesChanged;
+            session.PlaybackInfoChanged         += Session_PlaybackInfoChanged;
+            session.TimelinePropertiesChanged   += Session_TimelinePropertiesChanged;
+        }
+
+        private void Session_TimelinePropertiesChanged(GlobalSystemMediaTransportControlsSession sender, TimelinePropertiesChangedEventArgs args)
+        {
+            var time = sender.GetTimelineProperties();
+            var endTime = sender.GetTimelineProperties().EndTime;
+            var curTime = sender.GetTimelineProperties().Position;
+
+            var percentage = curTime.TotalSeconds / endTime.TotalSeconds;
+
+            this.Dispatcher.Invoke(() =>
+            {
+                TimeTotal.Content = String.Format("{0:D2}:{1:D2}", endTime.Minutes, endTime.Seconds);
+                TimeElapsed.Content = String.Format("{0:D2}:{1:D2}", curTime.Minutes, curTime.Seconds);
+
+                TimeProgress.Width = percentage * TimeProgress.MaxWidth;
+            });
+        }
+
+        private void Session_PlaybackInfoChanged(GlobalSystemMediaTransportControlsSession sender, PlaybackInfoChangedEventArgs args)
+        {
+            var info = sender.GetPlaybackInfo();
+            this.Dispatcher.Invoke(() =>
+            {
+                switch (info.PlaybackStatus)
+                {
+                    case GlobalSystemMediaTransportControlsSessionPlaybackStatus.Playing:
+                        PlaybackStatus.Content = "now playing";
+                        break;
+                    case GlobalSystemMediaTransportControlsSessionPlaybackStatus.Paused:
+                        PlaybackStatus.Content = "paused";
+                        break;
+                    case GlobalSystemMediaTransportControlsSessionPlaybackStatus.Stopped:
+                        PlaybackStatus.Content = "not playing";
+                        break;
+                }
+            });
+        }
+
+        private void Session_MediaPropertiesChanged(GlobalSystemMediaTransportControlsSession sender, MediaPropertiesChangedEventArgs args)
+        {
+            var info = sender.TryGetMediaPropertiesAsync().GetAwaiter().GetResult();
+
+            this.Dispatcher.Invoke(() =>
+            {
+                AlbumName.Content = info.AlbumTitle.ToUpper();
+                ArtistName.Content = info.Artist.ToUpper();
+                SongName.Content = info.Title;
+
+                //No Album Art Provided :/
+                if(info.Thumbnail == null)
+                {
+                    return;
+                }
+
+                var thumbnailStream = info.Thumbnail.OpenReadAsync().GetAwaiter().GetResult().AsStream();
+                BitmapImage bitmap = new BitmapImage();
+                bitmap.BeginInit();
+                bitmap.CacheOption = BitmapCacheOption.OnLoad;
+                bitmap.StreamSource = thumbnailStream;
+                bitmap.EndInit();
+
+                AlbumArt.Source = bitmap;
+            });
         }
     }
 }
